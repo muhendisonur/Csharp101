c19b715d7749efcef8df2eba6cae30f2297c1bbc
Onur YILDIZ <muhendisonuryildiz@gmail.com>
2023-05-18 12:33:05 +0300

    If ElseIf Ternary klasörü eklendi

---
+++ if-elseIf-Ternary/Program.cs
@@ -0,0 +1,12 @@
+﻿//günün saatine göre karşılama yapan program
+
+int zaman = DateTime.Now.Hour;
+// if(zaman >= 6 && zaman <= 11)
+//     Console.WriteLine("Günaydın!");
+// else if(zaman <= 18)
+//     Console.WriteLine("Tünaydın!");
+// else
+//     System.Console.WriteLine("İyi akşamlar!");
+
+//yukarıda ki if-else if-else yapısının ternary alternatifi
+Console.WriteLine((zaman >= 6 && zaman <= 11) ? "Günaydın!" : (zaman <= 18) ? "Tünaydın!" : "İyi akşamlar!");
