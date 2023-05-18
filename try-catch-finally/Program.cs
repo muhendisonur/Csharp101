6c755dfd1696e05699ccf8b5e3415f39ac7d9309
Onur YILDIZ <muhendisonuryildiz@gmail.com>
2023-05-18 12:03:35 +0300

    Try Catch Finally klasörü eklendi.

---
+++ try-catch-finally/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Sadece sayı ifadesi giriniz: ");
+try
+{
+    int girilenSayi = int.Parse(Console.ReadLine());
+    System.Console.WriteLine("Girdiğiniz sayı: " + girilenSayi + "'dır.");
+}
+catch(FormatException)
+{
+    Console.WriteLine("Hata: Sadece sayı giriniz!");
+}
+catch(Exception)
+{
+    System.Console.WriteLine("Hata, geliştiriciniz ile iletişime geçiniz.");
+}
+finally
+{
+    Console.WriteLine("Program sonlandı. İyi günler dileriz.");
+}
