4fa03246b526eb26fad0dc36d4f34042dcdd5a63
Onur YILDIZ <muhendisonuryildiz@gmail.com>
2023-05-18 22:19:14 +0300

    While Foreach klasörü eklendi

---
+++ for-break-continue/while-foreach/Program.cs
@@ -0,0 +1,11 @@
+﻿//foreach örneği
+string[] ogrenciler = {"Onur", "Yıldız", "Oğuzhan", "Kahraman"};
+Console.WriteLine("Öğrenciler: ");
+foreach(var item in ogrenciler)
+    Console.WriteLine(item + " ");
+
+//while örneği
+while(true)
+{
+    Console.WriteLine("Merhaba!");
+}
