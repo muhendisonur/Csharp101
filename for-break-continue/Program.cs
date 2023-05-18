edf9393f8ff9c8e89451a42c3601d34e67772579
Onur YILDIZ <muhendisonuryildiz@gmail.com>
2023-05-18 21:34:35 +0300

    For Break Continue klasörü eklendi

---
+++ for-break-continue/Program.cs
@@ -0,0 +1,20 @@
+﻿//girilen sayıya kadar tek sayıların toplamını yazan program
+
+int girilenDeger = 0;
+int toplam = 0;
+
+Console.WriteLine("Değer giriniz: ");
+try
+{
+    girilenDeger = int.Parse(Console.ReadLine());
+}
+catch(FormatException)
+{
+    Console.WriteLine("Sadece sayı ifadesi giriniz!");
+}
+
+for(int i = 0; i <= girilenDeger; i++)
+{
+    toplam += (i%2 != 0) ? i : 0;
+}
+Console.WriteLine("Toplam: " + toplam);
