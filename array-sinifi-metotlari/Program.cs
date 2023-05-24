a37096d4ba99f424ed66b12d7df90be11af1d6a6
Onur YILDIZ <muhendisonuryildiz@gmail.com>
2023-05-24 22:32:04 +0300

    Array Sınıfı Metotları klasörü eklendi

---
+++ array-sinifi-metotlari/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] sayilar = {5, 10, 15, 50, 61, 34, 54, 43, 81};
+
+//Sıralama
+System.Console.WriteLine("*************** Dizi Sıralandı ***************");
+foreach(var sayi in sayilar)
+    System.Console.WriteLine(sayi);
+
+//Silme
+System.Console.WriteLine("*************** Diziden Seçilen Eleman(lar)  Silindi ***************");
+Array.Clear(sayilar, 6, 3);
+foreach(var sayi in sayilar)
+    System.Console.WriteLine(sayi);
+
+//Ters Çevirme
+System.Console.WriteLine("*************** Dizi Sıralaması Tersine Çevirildi ***************");
+Array.Reverse(sayilar);
+foreach(var sayi in sayilar)
+    System.Console.WriteLine(sayi);
+
+//Elemanın İndeksini Bulmak
+System.Console.WriteLine("*************** 61 Sayısının İndeksi Bastırıldı ***************");
+System.Console.WriteLine(Array.IndexOf<int>(sayilar, 61));
+
+//Yeniden Boyutlandırma
+System.Console.WriteLine("*************** Dizi 15 Elemana Yükseltildi ***************");
+Array.Resize<int>(ref sayilar, 15);
+foreach(var sayi in sayilar)
+    System.Console.WriteLine(sayi);
