f91348a0008fcf76d53c25746b788fa455529d6c
Onur YILDIZ <muhendisonuryildiz@gmail.com>
2023-05-25 14:11:04 +0300

    Metot Overloading klasörü eklendi

---
+++ metot-overloading/Program.cs
@@ -0,0 +1,27 @@
+﻿//Out Kavramı Örnek
+Metotlar ornek = new Metotlar();
+ornek.topla(5, 6, out int toplam);
+System.Console.WriteLine(toplam);
+
+
+//Overloading
+ornek.kisiYazdir("onur");
+ornek.kisiYazdir("oğuzhan", "yıldız");
+
+class Metotlar
+{
+    public void topla(int sayi1, int sayi2, out int sonuc)
+    {
+        sonuc = sayi1 + sayi2;
+    }
+
+    public void kisiYazdir(string isim)
+    {
+        System.Console.WriteLine("Hoşgeldiniz {0}", isim);
+    }
+
+    public void kisiYazdir(string isim, string soyIsim) //2.parametre girilirse soyad olarak algılanıp tüm karakterler büyük harf yapılacak
+    {
+        System.Console.WriteLine("Hoşgeldiniz {0} {1}", isim, soyIsim.ToUpper());
+    }
+}
