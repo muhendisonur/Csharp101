32d00241647794ff8aab6a29dca39fbde27b0133
Onur YILDIZ <muhendisonuryildiz@gmail.com>
2023-06-06 21:31:55 +0300

    Sınıf Kavramı klasörü oluşturuldu

---
+++ sinif-kavrami/Program.cs
@@ -0,0 +1,22 @@
+﻿personel yazilimci = new personel(){
+    isim = "Onur",
+    soyad = "YILDIZ",
+    maas = 15000,
+    departman = "Ürün Geliştirme"
+};
+
+yazilimci.calis();
+
+
+class personel
+{
+    internal string isim;
+    internal string soyad;
+    internal int maas;
+    internal string departman;
+
+    internal void calis()
+    {
+        System.Console.WriteLine("{0} isimli personel olarak şuanda çalışmaya başladım!", this.isim);
+    }
+}
