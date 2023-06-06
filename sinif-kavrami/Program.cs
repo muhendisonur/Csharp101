ce9aea0ca07d4354821c9b2c72f275e68b02faae
Onur YILDIZ <muhendisonuryildiz@gmail.com>
2023-06-06 22:01:01 +0300

    Kurucu Metotlar konusu eklendi

--- sinif-kavrami/Program.cs
+++ sinif-kavrami/Program.cs
@@ -1,21 +1,30 @@
-﻿personel yazilimci = new personel(){
-    isim = "Onur",
-    soyad = "YILDIZ",
-    maas = 15000,
-    departman = "Ürün Geliştirme"
-};
+﻿personel yazilimci = new personel("Onur", "YILDIZ", 15000, "Ürün Geliştirme"); //instance
 
 yazilimci.calis();
 
-
 class personel
 {
+    //özellikler
     internal string isim;
     internal string soyad;
     internal int maas;
     internal string departman;
 
-    internal void calis()
+    internal personel() //kurucu metot
+    {
+        System.Console.WriteLine("Personel oluşturuldu!");
+    }
+
+    internal personel(string isimPara, string soyadPAra, int maasPara, string departmanPara) //kurucu metot
+    {
+        isim = isimPara;
+        soyad = soyadPAra;
+        maas = maasPara;
+        departman = departmanPara;
+    }
+
+    
+    internal void calis() //metot
     {
         System.Console.WriteLine("{0} isimli personel olarak şuanda çalışmaya başladım!", this.isim);
     }
