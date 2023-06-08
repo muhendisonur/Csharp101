064d17fc2583dc802eadcf48dbe9b8d402346aa7
Onur YILDIZ <muhendisonuryildiz@gmail.com>
2023-06-08 17:40:26 +0300

    Static Sınıf ve Üyeler konusu eklendi

--- sinif-kavrami/Program.cs
+++ sinif-kavrami/Program.cs
@@ -1,29 +1,42 @@
-﻿personel yazilimci = new personel("Onur", "YILDIZ", 8000, "Ürün Geliştirme"); //instance
+﻿personel.personelSayi(); //Sınıfı ilk çağırdığımız yer, static kurucu çalışır ve _personelSayısı 0 olarak atanır.
 
-yazilimci.calis();
+personel yazilimci1 = new personel("Onur", "YILDIZ", 25000, "Ürün Geliştirme"); //instance
+personel yazilimci2 = new personel("Ahmet", "Pınar", 8000, "İnsan Kaynakları");  //asgari ücretten düşük olduğu için personelsayisi değişkeni artırılmaz
+
+personel.personelSayi();
 
 class personel
 {
-    //özellikler
-    internal string isim;
-    internal string soyad;
+
+    private static int _personelSayisi; //sınıfın çağırıldığı tüm noktalarda ortak değer sunar.
+    public static int personelSayisi { get => _personelSayisi; }
+    private string isim; //field örneği
+    private string soyad;
     private int _maas; 
-    internal string departman;
+    private string departman;
 
     public int maas //property ile enkapsülasyon gerçekleştirildi
     {get => _maas; 
     set
     {
-        if(maas < 10927)
+        if(value < 10927)
+        {
             System.Console.WriteLine("Asgari ücretten düşük maaş veremezsin!");
+            _maas = 0;
+        }
+            
         else
+        {
             _maas = value;
+            _personelSayisi++;
+        }
+            
     }
     }
 
-    internal personel() //kurucu metot
+    static personel() //statik yapıcı metot çalışan sınıfına erişildiğinde bir kereye mahsus çalışır
     {
-        System.Console.WriteLine("Personel oluşturuldu!");
+        _personelSayisi = 0;
     }
 
     internal personel(string isimPara, string soyadPAra, int maasPara, string departmanPara) //kurucu metot
@@ -32,11 +45,17 @@ class personel
         soyad = soyadPAra;
         maas = maasPara;
         departman = departmanPara;
+            
     }
 
     
     internal void calis() //metot
     {
         System.Console.WriteLine("{0} isimli personel olarak şuanda çalışmaya başladım!", this.isim);
+    }
+
+    public static void personelSayi()
+    {
+        System.Console.WriteLine(_personelSayisi);
     }
 }
