bc4a1edb02d4eca3e88a367a6df36cbc89c8f8b5
Onur YILDIZ <muhendisonuryildiz@gmail.com>
2023-06-09 17:21:55 +0300

    Yapı ve Enum klasörleri eklendi

---
+++ enum-kavrami/Program.cs
@@ -0,0 +1,29 @@
+﻿long yillikCiro = 0;
+
+System.Console.WriteLine("Şirketinizin yıllık cirosunu giriniz: ");
+
+try
+{
+    yillikCiro = long.Parse(Console.ReadLine());
+}
+catch(FormatException)
+{
+    throw new FormatException("Sadece sayı giriniz!");
+}
+
+
+
+if(yillikCiro <= (int)sirketOlcegi.kucuk)
+    System.Console.WriteLine("Şirketiniz küçük ölçeklidir!");
+else if(yillikCiro <= (int)sirketOlcegi.orta)
+    System.Console.WriteLine("Şirketiniz orta ölçeklidir!");
+else
+    System.Console.WriteLine("Şirketiniz büyük ölçeklidir!");
+
+
+enum sirketOlcegi
+{
+    kucuk = 100000,
+    orta = 1000000,
+    buyuk = 1000000
+}
---
+++ struct-kavrami/Program.cs
@@ -0,0 +1,23 @@
+﻿kare ilkKare;
+ilkKare.kenarUzunlugu = 5;
+System.Console.WriteLine("{0} kenar uzunluğuna sahip karenin alanı {1}'dir!", ilkKare.kenarUzunlugu, ilkKare.alanHesapla());
+
+struct kare
+{
+    internal int kenarUzunlugu;
+
+    public kare()
+    {
+        kenarUzunlugu = 0;
+    }
+
+    public kare(int kenarUzunluguPara)
+    {
+        kenarUzunlugu = kenarUzunluguPara;
+    }
+
+    public int alanHesapla()
+    {
+        return kenarUzunlugu*kenarUzunlugu;
+    }
+}
